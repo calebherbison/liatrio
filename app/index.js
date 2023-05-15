599ec3f86f32be99e654ef7858be2c6c5a6e30d5
caleb <cherbison@coolerscreens.com>
2023-05-15 16:31:45 -0600

    initial commit

---
+++ app/index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const express = require('express');
+
+const PORT = 8000;
+const HOST = '0.0.0.0';
+
+const app = express();
+app.get('/', (req, res) => {
+  res.send(JSON.stringify({
+    message: "Automate all the things!",
+    timestamp: "1529729125"
+  }));
+});
+
+app.listen(PORT, HOST, () => {
+  console.log(`Running on http://${HOST}:${PORT}`);
+});
